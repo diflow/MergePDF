cc6f9e063d2a670ea5a6269d0adf5a5e9ecb78dc
Ivan Dvornyk <i.dvornyk@dotsplatform.com>
2022-04-06 14:32:23 +0300

    Initial Commit

---
+++ MergePDF/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MergePDF
+//
+//  Created by Ivan Dvornyk on 06.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
